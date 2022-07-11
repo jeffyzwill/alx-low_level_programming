2e649561f50a148fdaa2d56441ffa64a75c0161d
Jefferson Georgewill <jeffyzwill@gmail.com>
2022-07-10 21:35:50 -0700

    Update: Betty corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * This is a function prints and returns 0
+ * main - This is a function prints a text
+ * Return: 0 (which is a representation of void)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
