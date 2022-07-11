9d8a891f49b1ad26afbab6c4c69637cb71ff4fe7
Jefferson Georgewill <jeffyzwill@gmail.com>
2022-07-10 21:55:57 -0700

    C code that runs without errors

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This prints with printf function
+ * Return: This value should be null which corresponds with void
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
