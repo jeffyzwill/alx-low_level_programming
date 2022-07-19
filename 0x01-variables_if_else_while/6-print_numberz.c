18fc97804fd39210f23b877058e942215560d8bf
Jefferson Georgewill <jeffyzwill@gmail.com>
2022-07-19 04:32:26 -0700

    Mr Nice Watch

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all base 10 digits
+ * Return: 0
+ */
+
+int main(void)
+{
+	int let;
+
+	for (let = 48; let <= 57; let++)
+	{
+		putchar(let);
+	}
+	putchar('\n');
+	return (0);
+}
