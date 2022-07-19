9e86b5d32b7680b568f62f3ab8e9ec7c70d42695
Jefferson Georgewill <jeffyzwill@gmail.com>
2022-07-19 04:41:15 -0700

    I'm everywhere

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all lower case in reverse
+ * Return: 0
+ */
+
+int main(void)
+{
+	int let;
+
+	for (let = 122; (let = 122) && (let >= 97); let--)
+		putchar(let);
+	putchar(10);
+	return (0);
+}
