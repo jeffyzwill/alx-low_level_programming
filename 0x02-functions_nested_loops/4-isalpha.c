e884c9176112026b36ef2228dc690df6cb66d6c7
Jefferson Georgewill <jeffyzwill@gmail.com>
2022-07-23 01:51:38 -0700

    UPDATE: Betty correction

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,14 +2,14 @@
 
 /**
  * _isalpha - checks if a character is an alphabet
- * @c - Accepts a character
+ * @c: Accepts a character
  * Return: Either 1 or 0
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	if ((c >= 'A' && c <= i'Z') || (c >= 'a' && c <= 'z'))
 		return (1);
-	else 
+	else
 		return (0);
 }
