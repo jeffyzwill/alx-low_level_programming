2ac2fbbd5baaaa2878769f8c5f3af3aa57bd65a6
Jefferson Georgewill <jeffyzwill@gmail.com>
2022-07-31 11:57:11 -0700

    No dey take am

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Sums two numbers
+ * Return: the result of the sum
+ * @i: accepts an integer as argument
+ * @j: accepts an integer as argument
+ */
+
+int add(int i, int j)
+{
+	int k;
+
+	k = i + j;
+	return (k);
+}
