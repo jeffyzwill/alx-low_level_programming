78403dfb28ea598d67028d75ebb3375120013505
Jefferson Georgewill <jeffyzwill@gmail.com>
2022-08-10 05:44:53 -0700

    Index task 2: Added

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two digits
+ * @a: First integer to be multiplied
+ * @b: Second intger to be multiplied
+ * Return: The multiple of the two integers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
