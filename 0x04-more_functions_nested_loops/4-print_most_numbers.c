1fda82170751f98c4ae224381c246e81fbe295d8
Jefferson Georgewill <jeffyzwill@gmail.com>
2022-08-13 21:01:21 -0700

    Born sinner

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints numbers except some
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9';)
+	{
+		if ((n == '2') || (n == '4'))
+			n++;
+		else
+		{
+			_putchar(n);
+			_putchar('\n');
+			n++;
+		}
+	}
+}
